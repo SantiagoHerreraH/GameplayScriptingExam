b3853051b2dcf6da3ae2cd03a06df11b9079af47
SantiagoHerreraH <santi.herrerah.h@gmail.com>
2024-08-24 12:59:35 +0200

    Made screen size bigger

--- GD14_HerreraHuerta_Santiago_GameplayScripting/main.cpp
+++ GD14_HerreraHuerta_Santiago_GameplayScripting/main.cpp
@@ -12,7 +12,7 @@ int SDL_main(int argv, char** args)
 
 	StartHeapControl();
 
-	Game* pGame{ new Game{ Window{ "Project name - Name, first name - 1DAEXX", 846.f , 500.f } } };
+	Game* pGame{ new Game{ Window{ "Gameplay Scripting Exam - Herrera Huerta, Santiago - 1DAE14", 846.f * 1.5 , 500.f * 1.5} } };
 	pGame->Run();
 	delete pGame;
 
